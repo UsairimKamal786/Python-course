6aff7f8ffe873e0509ad5b5456b91b273f71a35d
Usairim Kamal <usairimkamal2016@gmail.com>
2025-05-30 10:52:49 +0500

    Create uppercase_lowercase_message_congratulations.py

---
+++ uppercase_lowercase_message_congratulations.py
@@ -0,0 +1,16 @@
+# This code converts a congratulation message to uppercase
+message = "congratulations"
+uppercase_message = message.upper()
+print(uppercase_message)
+
+# This code converts a congratulation letter "c" to uppercase
+message_2 = "ongratulations"
+letter_message = "c"
+update_c = letter_message.upper()
+print(update_c + message_2)
+
+# This code converts a congratulation letter "C" to lowercase
+message_3 = "ongratulations"
+letter_message = "C"
+update_letter_c = letter_message.lower()
+print(update_letter_c + message_3)
