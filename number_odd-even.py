5f15ea7bc9862ef0b92c517870288a6ce335cfbc
Usairim Kamal <usairimkamal2016@gmail.com>
2025-06-13 09:45:48 +0500

    Create number_odd-even.py

---
+++ number_odd-even.py
@@ -0,0 +1,6 @@
+number = int(input("Enter Number to check: "))
+print("Number to be Checked: ", number)
+if number % 2 == 0:
+    print("This is an Even Number")
+else:
+    print("This is an Odd Number")
